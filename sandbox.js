84eb5b45d384c150e7c692d5250cf4fd596eab6e
Mehran Taghavi <M.Taghavi@farabi.local>
2025-04-09 09:27:16 +0330

    delet trush code in file

--- sandbox.js
+++ sandbox.js
@@ -1,8 +1,24 @@
-const closeBttn=document.getElementById('close-btn')
+// const closeBttn=document.getElementById('close-btn')
 
 
 
-addEventListener('click'(e)=>{
+// addEventListener('click'(e)=>{
 
-})
+// })
 
+
+const toggleLink = document.getElementById('mor-faq');
+const targetElement = document.getElementById('btn-faq');
+
+toggleLink.addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    
+    if (targetElement.classList.contains('highlight')) {
+       
+        targetElement.classList.remove('highlight');
+    } else {
+      
+        targetElement.classList.add('highlight');
+    }
+});
